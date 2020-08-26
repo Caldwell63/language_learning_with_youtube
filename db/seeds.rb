8b131c81685ddba39d80352cc5cd9d9778eb6092
Anton Voronin <auvoronin@gmail.com>
2020-08-26 20:05:10 +0200

    Some chamges

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'tts'
+# Function that grabs the user input and plays it in English
+# And then displays what was said in the command line.
+# Allows us to easily just write "say()"
+def say(word)
+    s = "#{word}"
+    repeatTimes = 1
+    s.play("en", repeatTimes)
+    puts
+    puts "Successfully Said: #{word}"
+end
+
+say("anton hello")
