30b75c3571eeb833e22f0c0cf26ef7634abbb0c7
Anton Voronin <auvoronin@gmail.com>
2020-08-28 12:40:00 +0200

    clean up folder shared

--- app/assets/images/addbadge.js
+++ app/assets/images/addbadge.js
@@ -1,13 +1,17 @@
-//
-// chrome.browserAction.onClicked.addListener((tab) => {
-//   chrome.tabs.executeScript({
-//     code: 'document.body.style.backgroundColor="#C3413B"'
-//   });
-// });
+
+chrome.browserAction.onClicked.addListener((tab) => {
+  chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor="#C3413B"'
+           // 'document.body.style.backgroundColor="#C1987B"'
+
+
+    // const list = document.querySelector('ytd-video-primary-info-renderer h1');
+  });
+});
 
 console.log("Hello")
 
-// const d1 = document.querySelector('ytd-video-primary-info-renderer h1');
+
 //
 // console.log(d1);
 // d1.insertAdjacentHTML('afterend', '<div id="two">two</div>');
