229047ff130e7c75d8943a2aca1971647d925da6
Alexander Volz <alex1volz@gmail.com>
2020-09-01 10:10:43 +0200

    done

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,11 +2,13 @@ require 'net/http'
 require 'json'
 require 'awesome_print'
 
+
 class Card < ApplicationRecord
   belongs_to :user
   belongs_to :word
+  api_key = ENV['GIFY_API_KEY']
 
-  url = "http://api.giphy.com/v1/gifs/search?q=potato&api_key=ZHywSz17b2c6ZeCPWYFz8HTEPJhCtqyR&limit=1"
+  url = "http://api.giphy.com/v1/gifs/search?q=potato&api_key=#{api_key}&limit=1"
   resp = Net::HTTP.get_response(URI.parse(url))
   buffer = resp.body
   result = JSON.parse(buffer)
