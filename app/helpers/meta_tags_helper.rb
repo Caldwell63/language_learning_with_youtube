be6d9864100b66b697de303aed14d450ff2dfe96
eduina oliveira <oliveirateduina@gmail.com>
2020-09-02 08:28:23 +0200

    newchanges

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -7,13 +7,10 @@ module MetaTagsHelper
       content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["meta_description"]
     end 
 
-    def meta_video
-      content_for?(:meta_video) ? content_for(:meta_video) :  DEFAULT_META["meta_video"]
-    end
-
     def meta_image
       meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["meta_image"])
       # little twist to make it work equally with an asset or a url
       meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
     end
+    
 end
