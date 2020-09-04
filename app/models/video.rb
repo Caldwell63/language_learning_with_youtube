c2dd0ae5e6b4530f5f728cc5c9c4a063b896dd75
Alexander Volz <alex1volz@gmail.com>
2020-09-04 10:45:36 +0200

    front and api

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,12 +2,10 @@ class Video < ApplicationRecord
   include PgSearch::Model
   # validates :youtube_id, uniqueness: true
 
-  scope :level_1, -> { where(level: 1..500) }
-  scope :level_2, -> { where(level: 500..1000) }
-  scope :level_3, -> { where(level: 1000..1500) }
-  scope :level_4, -> { where(level: 1500..2000) }
-  scope :level_5, -> { where(level: 2000..3000) }
-  scope :level_6, -> { where(level: 3000..5000) }
+  scope :level_1, -> { where(level: 1..199) }
+  scope :level_2, -> { where(level: 200..319) }
+  scope :level_3, -> { where(level: 320..1500) }
+  scope :level_4, -> { where(level: 1..1500) }
 
   pg_search_scope :search,
     against: [:title, :level],
